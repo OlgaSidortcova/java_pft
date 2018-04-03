de12756dd543415346f83985e0dd05f8fc4bc003
SadSido <sadsido@sadsido-Aspire-5738.(none)>
2018-04-03 15:13:13 +0300

    added some functions

---
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/MyFirstProgram.java
@@ -0,0 +1,35 @@
+package ru.stqa.ptf.sandbox;
+
+public class MyFirstProgram {
+
+  public static void main(String[] args) {
+
+    hello("World");
+    hello("user");
+    hello("Olga");
+
+
+    double l = 8;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+    double a = 3;
+    double b = 7;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и "+ b +" = " + area(a,b));
+  }
+
+  public static void hello ( String somebody){
+    //String somebody = "World";
+    System.out.println("Hello, " + somebody + "!");
+
+  }
+
+  public static double area(double len){
+    return len*len;
+
+  }
+
+  public static double area (double a, double b){
+    return a*b;
+
+  }
+}
