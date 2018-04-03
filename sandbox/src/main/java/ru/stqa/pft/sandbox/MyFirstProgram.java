a51f95aa2533900231b56c681ea479845af7ef26
SadSido <sadsido@sadsido-Aspire-5738.(none)>
2018-04-03 16:02:12 +0300

    Пофикшена ошибка с путаницей pft и ptf

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,33 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, my World");
+
+    hello("World");
+    hello("user");
+    hello("Olga");
+
+
+    double l = 8;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+    double a = 3;
+    double b = 7;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и "+ b +" = " + area(a,b));
   }
 
+  public static void hello ( String somebody){
+    //String somebody = "World";
+    System.out.println("Hello, " + somebody + "!");
+
+  }
+
+  public static double area(double len){
+    return len*len;
+
+  }
+
+  public static double area (double a, double b){
+    return a*b;
+
+  }
 }
