d9fb31d4cf7bb2826e03ba53c58269d2df40128e
SadSido <sadsido@sadsido-Aspire-5738.(none)>
2018-04-08 00:35:04 +0300

    Задание №4: Создан класс CreateNewContactTests для тестирования добавления контактов в адресную книгу. Использован Selenium и вспомогательный класс NewContactData.

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/NewContactData.java
@@ -0,0 +1,61 @@
+package ru.stqa.pft.addressbook;
+
+public class NewContactData {
+  private final String first_name;
+  private final String last_name;
+  private final String company;
+  private final String address;
+  private final String home;
+  private final String mobile;
+  private final String work;
+  private final String fax;
+  private final String email;
+
+  public NewContactData(String first_name, String last_name, String company, String address, String home, String mobile, String work, String fax, String email) {
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.company = company;
+    this.address = address;
+    this.home = home;
+    this.mobile = mobile;
+    this.work = work;
+    this.fax = fax;
+    this.email = email;
+  }
+
+  public String getFirst_name() {
+    return first_name;
+  }
+
+  public String getLast_name() {
+    return last_name;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getHome() {
+    return home;
+  }
+
+  public String getMobile() {
+    return mobile;
+  }
+
+  public String getWork() {
+    return work;
+  }
+
+  public String getFax() {
+    return fax;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+}
