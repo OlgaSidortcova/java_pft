34a727e33b35e3e82d84f500a085789cfce4a0a7
Olga_Sidortcova <olga-24_@mail.ru>
2018-06-07 15:15:21 +0300

    Реализована возможность удаленнного запуска браузера.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -24,7 +24,7 @@ public class TestBase {
 
   Logger logger = LoggerFactory.getLogger(TestBase.class);
   protected static final ApplicationManager app = new ApplicationManager(
-          System.getProperty("browser", BrowserType.FIREFOX));
+          System.getProperty("browser", BrowserType.CHROME));
 ////vot tut problemy
   @BeforeSuite
   public void setUp() throws Exception {
